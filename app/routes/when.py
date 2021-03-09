417409f3c400d6ffb9303cb1f81b760fdc783bb9
David Migoya <dmigoya@gmail.com>
2021-03-09 19:22:35 +0300

    fixed single  retreaval of data

--- app/routes/when.py
+++ app/routes/when.py
@@ -3,11 +3,11 @@ from fastapi.encoders import jsonable_encoder
 
 from app.server.database import (
     retreive_moods,
-    retreive_moods_with_details, add_mood,
+    retreive_mood_with_details, add_mood,
     
 )
 from app.server.models.when import (
-    WhenSchema, FullReading, ResponseModel
+    WhenSchema, FullReading, ResponseModel, ErrorResponseModel
     
 )
 
@@ -16,22 +16,23 @@ from app.server.models.when import (
 
 router = APIRouter()
 
-
+# Get all the Feelings/Moods witthout the readings
 @router.get("/", response_description ="All Moods are retreived")
-async def get_moods ():
+async def get_moods():
     moods = await retreive_moods()
     if moods:
-        return WhenSchema
-    return "Error occured or Empty model"
+        return  ResponseModel(moods, "All feelings/moods retreived successfully")
+    return ResponseModel(moods, "Empty list returned")
 
 
-
+# Get single mood/Feeling with readings
 @router.get("/{id}", response_description="Single mood is retreived")       
 async def get_one_with_details(id):
-    one_mood  = await retreive_moods_with_details(id)
+    one_mood  = await retreive_mood_with_details(id)
     if one_mood:
-        return  FullReading( one_mood, "Single Data received succesfully")
-    return "404 Error occured"
+        return  ResponseModel( one_mood, "Single Reading received succesfully")
+    return ErrorResponseModel("An error occurred.", 404, "Student doesn't exist.")
+
 
 @router.post("/", response_description=" reading added successfully")
 async def add_reading( readings: FullReading = Body(...)): 
--- app/server/database.py
+++ app/server/database.py
@@ -3,28 +3,23 @@ import os
 from bson.objectid import ObjectId
 from .models.when import FullReading, WhenSchema
 
-# mongo_url = os.getenv(MONGODB_URL)
-# Defined .env path
-# env_path = Path(__file__).parent.parent / '.env'
-# load_dotenv(verbose=True, dotenv_path=env_path)
-
+ 
 MONGO_DETAILS = "mongodb://localhost:27017"
 
 client = m_async.AsyncIOMotorClient(MONGO_DETAILS)
 db = client.bibleVerses_db
 
-when_Collection = db.get_collection("when_collection")
 full_Collection = db.get_collection("full_reading_collection")
 
 # helpers
-def single_when_helper(when: WhenSchema ) -> dict:
+def all_moods_helper(reading :FullReading ) -> dict:
     return {
-        "id": str(when["_id"]),
-        "title": when["title"],
-         "description": when["description"],
+        "id": str(reading["_id"]),
+        "title": reading["when"]["title"],
+         "description": reading["when"]["description"],
     }
 
-def full_reading_helper(reading :FullReading) -> dict:
+def reading_helper(reading :FullReading) -> dict:
     return {
         "id": str(reading["_id"]),
         "when": reading["when"],
@@ -37,19 +32,19 @@ def full_reading_helper(reading :FullReading) -> dict:
 # Get all Moods/Situatons
 async def retreive_moods():
     moods = []
-    async for mood in when_Collection.find():
-        moods.append(single_when_helper(mood))
+    async for mood in full_Collection.find():
+        moods.append(all_moods_helper(mood))
     return moods
 
-# Get all moods with  readings
-async def retreive_moods_with_details(id: str) -> dict:
+# Get one Feeling with  reading details
+async def retreive_mood_with_details(id: str) -> dict:
      mood = await full_Collection.find_one({"_id": ObjectId(id)})
      if  mood:
-         return full_reading_helper(mood)
+         return reading_helper(mood)
     
 # Add mood to Database
 
 async def add_mood( fullReading: FullReading) -> dict:
     readings = await  full_Collection.insert_one(fullReading)
     new_mood = await full_Collection.find_one({"_id": readings.inserted_id})
-    return  full_reading_helper(new_mood)
+    return  reading_helper(new_mood)
